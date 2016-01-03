be610ca512b97b596147122e98914ebfb3542feb
evisar <evisar@gmail.com>
2016-01-03 19:35:56 +0100

    Couple of small fixes in the interface inheritance

--- ddd/sample/countries/services/ICountryService.cs
+++ ddd/sample/countries/services/ICountryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ddd.common;
+using ddd.integration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ddd.sample.countries.services
 {
-    public interface ICountryService
+    public interface ICountryService : IIntegrationService<Country>
     {
         Country FindById(Guid id);
 
